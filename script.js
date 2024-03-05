d2af41f21dd24b7b53e63b6625da4285d23205db
LeeRu3 <137153120+LeeRu3@users.noreply.github.com>
2024-03-05 08:51:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ document.addEventListener('DOMContentLoaded', function () {
         folder.addEventListener('click', function () {
             const nestedList = folder.parentElement.querySelector('ul');
             const topLevel = folder.closest('.file-tree').querySelectorAll('.folder span');
-            if (folder.parentElement.classList.contains('open')) {
-                folder.parentElement.classList.remove('open');
-                nestedList.style.display = 'none';
-            } else {
-                topLevel.forEach(function (topFolder) {
-                    const topNestedList = topFolder.parentElement.querySelector('ul');
-                    topFolder.parentElement.classList.remove('open');
-                    topNestedList.style.display = 'none';
-                });
-                folder.parentElement.classList.add('open');
-                nestedList.style.display = 'block';
+            const isChildOfGrafika = folder.closest('.folder').classList.contains('Grafika');
+
+            if (!isChildOfGrafika) {
+                if (folder.parentElement.classList.contains('open')) {
+                    folder.parentElement.classList.remove('open');
+                    nestedList.style.display = 'none';
+                } else {
+                    topLevel.forEach(function (topFolder) {
+                        const topNestedList = topFolder.parentElement.querySelector('ul');
+                        topFolder.parentElement.classList.remove('open');
+                        topNestedList.style.display = 'none';
+                    });
+                    folder.parentElement.classList.add('open');
+                    nestedList.style.display = 'block';
+                }
             }
         });
     });
