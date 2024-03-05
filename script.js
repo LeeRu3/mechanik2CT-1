972ec6207552306288d978c847b87c19ba12d523
LeeRu3 <137153120+LeeRu3@users.noreply.github.com>
2024-03-05 08:48:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     folders.forEach(function (folder) {
         folder.addEventListener('click', function () {
             const nestedList = folder.parentElement.querySelector('ul');
-            folder.parentElement.classList.toggle('open');
-            nestedList.style.display = nestedList.style.display === 'none' ? 'block' : 'none';
+            const topLevel = folder.closest('.file-tree').querySelectorAll('.folder span');
+            if (folder.parentElement.classList.contains('open')) {
+                folder.parentElement.classList.remove('open');
+                nestedList.style.display = 'none';
+            } else {
+                topLevel.forEach(function (topFolder) {
+                    const topNestedList = topFolder.parentElement.querySelector('ul');
+                    topFolder.parentElement.classList.remove('open');
+                    topNestedList.style.display = 'none';
+                });
+                folder.parentElement.classList.add('open');
+                nestedList.style.display = 'block';
+            }
         });
     });
 });
